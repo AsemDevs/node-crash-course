1342957a4d679779c5b69bc8513d58a220dc2500
Asem Hamdi <asemhamdi97@gmail.com>
2023-04-10 19:37:51 +0300

    timeConsumed

--- plyground.js
+++ plyground.js
@@ -1,12 +1,9 @@
-const person = {
-    name: 'John',
-    age: 30,
-    greet: function() {
-      setTimeout(() => {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-      }, 100);
-    }
-  };
-  
-  person.greet(); // logs "Hello, my name is John and I am 30 years old." after 1 second
-  
+const startTime = new Date();
+
+const timeConsumed = () => {
+    const endTime = new Date();
+    const timeElapsed = endTime - startTime;
+    console.log(`Done After ${timeElapsed}`);
+};
+const delay = 3000;
+setTimeout(timeConsumed, delay);
